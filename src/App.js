8aa7077cbfca2e9524383d36c94ff4f3d64ee1a7
Fazliddin-TM <dasturchiiio8o7@gmail.com>
2021-12-18 13:39:29 +0300

    mastert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Home from "./home";
+import About from "./about";
+import Service from "./service";
+import Project from "./project";
+import Contact from "./contact";
+import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <About />
+      <Service />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
   );
 }
